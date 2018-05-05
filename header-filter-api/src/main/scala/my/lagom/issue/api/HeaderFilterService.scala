a495ce0a3749161ff558e597a5376f59d31f7fba
fernando.hackbart <fernando.hackbart@timestamp.pt>
2018-05-05 21:28:45 +0100

    Initial version

--- header-filter-api/src/main/scala/my/lagom/issue/api/HeaderFilterService.scala
+++ header-filter-api/src/main/scala/my/lagom/issue/api/HeaderFilterService.scala
@@ -3,9 +3,7 @@ package my.lagom.issue.api
 import akka.NotUsed
 import com.lightbend.lagom.scaladsl.api.transport.Method
 import com.lightbend.lagom.scaladsl.api.{Service, ServiceCall}
-import javax.inject.Inject
 import my.lagom.issue.code.MyConfigHeaderFilter
-import play.api.Configuration
 
 trait HeaderFilterService extends Service {
 
